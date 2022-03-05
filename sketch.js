ff1e02c94282af1feba796f5cd520ed463663d70
CristianoLucas122 <101006564+CristianoLucas122@users.noreply.github.com>
2022-03-05 11:24:01 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,41 @@
+var caixa 
+
+
+
+
+function setup() {
+  createCanvas(400,400);
+  caixa = createSprite(200,200,30,30);
+  caixa.shapeColor= "red";
+}
+
+function draw() 
+{
+  background(30);
+
+if(keyDown(RIGHT_ARROW)){
+caixa.position.x= caixa.position.x+ 5;
+
+}
+if(keyDown(LEFT_ARROW)){
+  caixa.position.x= caixa.position.x- 5;
+  
+  }
+  if(keyDown(UP_ARROW)){
+    caixa.position.y= caixa.position.y- 5;
+    
+    }
+    if(keyDown(DOWN_ARROW)){
+      caixa.position.y= caixa.position.y+ 5;
+      
+      }
+
+
+
+
+drawSprites();
+}
+
+
+
+
